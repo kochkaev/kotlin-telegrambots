3bf772d9af93126088d25fc80162527722167fc4
dmitrii-kochkaev <edukisel@yandex.ru>
2026-01-11 22:01:30 +0700

    fix: Give check-for-updates.yml workflow permission to write in repo.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,6 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.35.0"
 }
 
-mavenPublishing {
-    coordinates("io.github.kochkaev", "kotlin-telegrambots", "${property("projectVersion") as String}+${property("telegrambotsVersion") as String}")
-}
-
 allprojects {
     group = "io.github.kochkaev"
     version = "${property("projectVersion") as String}+${property("telegrambotsVersion") as String}"
